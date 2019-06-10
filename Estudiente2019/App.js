d5ed70b58686a9cd7690faf261041253fe8b47cb
jiortiz <javier.ortiz.14@sansano.usm.cl>
2019-06-10 11:06:06 -0400

    Prueba de modal y Intento de separar
    ahora vamos a separar la app en un css y en otros js para modularizar

--- Estudiente2019/App.js
+++ Estudiente2019/App.js
@@ -8,7 +8,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import { AppRegistry, Image } from 'react-native';
+import {AppRegistry, Image } from 'react-native';
+import {Modal, TouchableHighlight, Alert} from 'react-native';
 
 /*const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -27,6 +28,50 @@ export default class App extends Component<Props> {
     );
   }
 }*/
+export default class ModalExample extends Component {
+  state = {
+    modalVisible: false,
+  };
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+  render() {
+    return (
+      <View style={{marginTop: 22}}>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}>
+          <View style={{marginTop: 22}}>
+            <View>
+              <Text>Hello World!</Text>
+
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <Text>Hide Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
+        <TouchableHighlight
+          onPress={() => {
+            this.setModalVisible(true);
+          }}>
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
 
 export default class Placeholder extends Component {
   render() {
@@ -34,12 +79,12 @@ export default class Placeholder extends Component {
       uri: './images/placeholder.png'
     };
     return (
-      <Image source={require('./images/placeholder.png')} style={{width: 193, height: 110}}/>
+        <Image source={require('./images/placeholder.png')} style={{width: 193, height: 110}}/>
     );
   }
 }
 
-// No se que hace esto:
+// No se que hace esto: Esto deberia ir en un css
 AppRegistry.registerComponent('Estudiente2019', () => Placeholder);
 
 const styles = StyleSheet.create({
