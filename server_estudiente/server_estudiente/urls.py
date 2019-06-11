5c25aa9337b0eee78137b5f7b11ce8bedd71a091
asantiba <alexis.santibanez.14@sansano.usm.cl>
2019-06-10 20:01:29 -0400

    nueva vista index en el servidor

--- server_estudiente/server_estudiente/urls.py
+++ server_estudiente/server_estudiente/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from App import views
+
 
 urlpatterns = [
+    url(r'^index/', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
