c70155b9ec6502f70d92c0833bc6e09062d7fc8b
jiortiz <javier.ortiz.14@sansano.usm.cl>
2019-06-13 02:27:28 -0400

    Modelo 3d
    Componentes.... uso ver en la pag. www.npmjs.com/package/react-native-gl-model-view

---
+++ Estudiente2019/Ficha.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, ScrollView} from 'react-native';
+import {AppRegistry, Image } from 'react-native';
+
+
+
+export default class FichaPaciente extends Component {
+
+    render() {
+        return (
+
+            <View>
+                <Text>
+                    Ficha Peciente
+                </Text>
+                <ScrollView>
+                    <View>
+                        <View>
+                            <View>
+                            </View>
+                        </View>
+                    </View>
+                </ScrollView>
+            </View>
+
+        );
+    }
+}
