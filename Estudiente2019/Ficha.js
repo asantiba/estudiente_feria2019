2360081df050fdee650f1efc0e3c564e2a7056a8
jiortiz <javier.ortiz.14@sansano.usm.cl>
2019-06-15 14:33:46 -0400

    Revert "pusheo previo al merge"
    This reverts commit f88c8752689bea2b6db4ea183d322dcd30105e88.

--- Estudiente2019/Ficha.js
+++ Estudiente2019/Ficha.js
@@ -1,42 +1,26 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, ScrollView} from 'react-native';
-import {AppRegistry, Image, Dimensions } from 'react-native';
+import {AppRegistry, Image } from 'react-native';
 
 
 
 export default class FichaPaciente extends Component {
-
-    const SCREEN_WIDTH = Dimensions.get('window').width;
-    const SCREEN_HEIGHT = Dimensions.get('window').height;
 
     render() {
         return (
 
             <View>
                 <Text>
-                    Ficha Tratamiendo
+                    Ficha Peciente
                 </Text>
-
                 <ScrollView>
-
                     <View>
-                        <Text>
-                            Esta ficha solo muestra la informacion del tratamiento en proceso
-                        </Text>
                         <View>
                             <View>
-
                             </View>
                         </View>
                     </View>
                 </ScrollView>
-                <TouchableOpacity
-                onPress={() => {}}
-                style={{justifyContent:'center', alignItems: 'center'}}>
-                    <Text style={{color: '#f0f8ff', fontWeight: 'bold', fontSize: 30}}>
-                        Entendido
-                    </Text>
-                </TouchableOpacity>
             </View>
 
         );
--- Estudiente2019/__tests__/Dien-test.js
+++ Estudiente2019/__tests__/Dien-test.js
@@ -7,105 +7,55 @@ class dien-test extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, flexDirection: "row" , alignItems: 'center', justifyContent: 'center' }}>
-        switch (this.props.idPiezaDental){
-                    case '11':
-                        this.imgPieza= require('./images/DientesImg/iu1.png');
-                        break;
-                    case '12':
-                        this.imgPieza= require('./images/DientesImg/iu2.png');
-                        break;
-                    case '13':
-                        this.imgPieza= require('./images/DientesImg/iu3.png');
-                        break;
-                    case '14':
-                        this.imgPieza= require('./images/DientesImg/iu4.png');
-                        break;
-                    case '15':
-                        this.imgPieza= require('./images/DientesImg/iu5.png');
-                        break;
-                    case '16':
-                        this.imgPieza= require('./images/DientesImg/iu6.png');
-                        break;
-                    case '17':
-                        this.imgPieza= require('./images/DientesImg/iu7.png');
-                        break;
-                    case '18':
-                        this.imgPieza= require('./images/DientesImg/iu8.png');
-                        break;
-                    case '21':
-                        this.imgPieza= require('./images/DientesImg/u1.png');
-                        break;
-                    case '22':
-                        this.imgPieza= require('./images/DientesImg/u2.png');
-                        break;
-                    case '23':
-                        this.imgPieza= require('./images/DientesImg/u3.png');
-                        break;
-                    case '24':
-                        this.imgPieza= require('./images/DientesImg/u4.png');
-                        break;
-                    case '25':
-                        this.imgPieza= require('./images/DientesImg/u5.png');
-                        break;
-                    case '26':
-                        this.imgPieza= require('./images/DientesImg/u6.png');
-                        break;
-                    case '27':
-                        this.imgPieza= require('./images/DientesImg/u7.png');
-                        break;
-                    case '28':
-                        this.imgPieza= require('./images/DientesImg/u8.png');
-                        break;
-                    case '31':
-                        this.imgPieza= require('./images/DientesImg/iD1.png');
-                        break;
-                    case '32':
-                        this.imgPieza= require('./images/DientesImg/iD2.png');
-                        break;
-                    case '33':
-                        this.imgPieza= require('./images/DientesImg/iD3.png');
-                        break;
-                    case '34':
-                        this.imgPieza= require('./images/DientesImg/iD4.png');
-                        break;
-                    case '35':
-                        this.imgPieza= require('./images/DientesImg/iD5.png');
-                        break;
-                    case '36':
-                        this.imgPieza= require('./images/DientesImg/iD6.png');
-                        break;
-                    case '37':
-                        this.imgPieza= require('./images/DientesImg/iD7.png');
-                        break;
-                    case '38':
-                        this.imgPieza= require('./images/DientesImg/iD8.png');
-                        break;
-                    case '41':
-                        this.imgPieza= require('./images/DientesImg/D1.png');
-                        break;
-                    case '42':
-                        this.imgPieza= require('./images/DientesImg/D2.png');
-                        break;
-                    case '43':
-                        this.imgPieza= require('./images/DientesImg/D3.png');
-                        break;
-                    case '44':
-                        this.imgPieza= require('./images/DientesImg/D4.png');
-                        break;
-                    case '45':
-                        this.imgPieza= require('./images/DientesImg/D5.png');
-                        break;
-                    case '46':
-                        this.imgPieza= require('./images/DientesImg/D6.png');
-                        break;
-                    case '47':
-                        this.imgPieza= require('./images/DientesImg/D7.png');
-                        break;
-                    case '48':
-                        this.imgPieza= require('./images/DientesImg/D8.png');
-                        break;
-
-                  };
+        <View style={{ flex: 1, flexDirection: "column" , alignItems: 'center', justifyContent: 'center' }}>
+            <View>
+                <Image source={require('./images/DientesImg/iu1.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iu2.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iu3.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iu4.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iu5.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iu6.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iu7.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iu8.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iD8.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iD7.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iD6.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iD5.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iD4.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iD3.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iD2.png')} style={{height: 50, width: 50}}/>
+            </View>
+            <View>
+                <Image source={require('./images/DientesImg/iD1.png')} style={{height: 50, width: 50}}/>
+            </View>
         </View>
 
 
